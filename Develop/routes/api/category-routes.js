c08fce91979c761e845b84abcbb6a883b3c74389
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-11-21 11:14:20 -0500

    post delete routes for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -34,16 +34,52 @@ catch (err) {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const DataCategory = await Category.create(req.body);
+    res.status(200).json(DataCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const DataCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!DataCategory[0]) {
+      res.status(404).json({ message: 'No category found!'});
+      return;
+    }
+    res,status(200).json(DataCategory);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const DataCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!DataCategory) {
+      res.status(404).json({ message: "No category found!"});
+      return;
+    }
+    res.status(200).json(DataCategory);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
