6d96d61858ce680fddbdd989b5226d8ba7a407dc
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-11-21 00:12:59 -0500

    category routes gor GET

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const DataCategory = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(DataCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+try {
+  const DataCategory = await Category.findByPk(re.params.id, {
+    include: [{ model: Product}]
+  }),
+  if (!DataCategory) {
+    res.status(404).json ({ message: "NO CATEGORY FOUND!"});
+    return;
+  }
+  res.status(200).json(DataCategory);
+} 
+catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
